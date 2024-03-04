972dc2d57084c1833f518a9fa9dffbfc4e8e5f6a
daniel <ogeradaniel933@gmail.com>
2024-03-04 12:46:00 +0300

    committing script/js and style.css after editting

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
-let notes = document.querySelector(".input-box");
-
 
 function showNotes(){
 	notesContainer.innerHTML = localStorage.getItem("notes");
 }
-// local storage (storing data in the browser)
 
 function updateStorage(){
 	localStorage.setItem("notes", notesContainer.innerHTML);
 }
-createBtn.addEventListener("click", ()=>{
+
+createBtn.addEventListener("click", () => {
 	let inputBox = document.createElement("p");
 	let img = document.createElement("img");
 	inputBox.className = "input-box";
 	inputBox.setAttribute("contenteditable", "true");
 	img.src = "images/circle.png";
 	notesContainer.appendChild(inputBox).appendChild(img);
+	updateStorage();
+});
 
-})
-
-notesContainer.addEventListener("click", function(e){
-	if(e.target.tagName === "IMG"){
+notesContainer.addEventListener("click", function(e) {
+	if (e.target.tagName === "IMG") {
 		e.target.parentElement.remove();
 		updateStorage();
+	} else if (e.target.tagName === "P") { // Corrected the tag name comparison and fixed syntax
+		let inputBox = e.target;
+		inputBox.onkeyup = function() {
+			updateStorage();
+		};
 	}
-	else if(e.target.tagName == "p"{
-		notesContainer = document.querySelectorAll(".inputBox");
-		notes.forEach(nt => {
-			nt.onkeyup = function(){
-				updateStorage();
-			}
-		})
-	})
+});
 
-})
+// Call showNotes() to display any existing notes when the page loads
+showNotes();
+document.addEventListener("keydown", event =>{
+	if( event.key === "Enter"){
+		document.execCommand("insertLineBreak");
+		event.preventDefault();
+	}
 
-
+});
